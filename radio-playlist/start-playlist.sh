c7043a34afdcf77e787bf88ba894ada7560db999
Charles Moulliard <cmoulliard@redhat.com>
2019-01-01 19:22:38 +0100

    Add bash script

---
+++ radio-playlist/start-playlist.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+
+volumio=localhost:3000/api/v1/commands
+until $(curl --silent --output /dev/null --head --fail ${volumio}); do
+   echo "We wait till volumio is up and running"
+   sleep 10s
+done
+
+sleep 20s
+echo "Volumio server is running, so we can launch our playlist"
+curl localhost:3000/api/v1/commands/?cmd='playplaylist&name=PL_Classic21'
